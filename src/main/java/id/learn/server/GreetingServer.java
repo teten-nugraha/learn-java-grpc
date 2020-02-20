768260f25dc0ce8c1381bf47880487f5b0314cb3
Teten Nugraha <teten.nugraha@allianz.co.id>
2020-02-20 07:51:47 +0700

    update

---
+++ src/main/java/id/learn/server/GreetingServer.java
@@ -0,0 +1,27 @@
+package id.learn.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class GreetingServer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Hello GRPC ");
+
+        final Server server = ServerBuilder.forPort(50581)
+                .build();
+
+        server.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Received shutdown request");
+            server.shutdown();
+            System.out.println("Successfully shutdown server");
+        }));
+
+        server.awaitTermination();
+    }
+
+}
